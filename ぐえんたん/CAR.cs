281118010e8dc78c9eed349c7c9d5ce8e4da8f5e
thanangn <thanh7313@gmail.com>
2020-09-01 14:18:03 +0900

    thanh

---
+++ ぐえんたん/CAR.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GOGOGO
+{
+    public class CAR
+    {
+        public float Speed;
+        public float AttackPower;
+        public float DefencePower;
+        public float HP;
+
+        public CAR(float speed, float hp , float attackPower, float defencePower)
+        {
+            this.Speed = speed;
+            this.AttackPower = attackPower;
+            this.DefencePower = defencePower;
+            this.HP = hp;
+        }
+
+        public float Position = 0f;
+
+
+        public void Run(float time)
+        {
+            Position = Speed * time;
+        }
+
+
+        public void Attack(CAR target)
+        {
+            target.HP -= DamageCalculator.CaclDamage(this, target);
+        }
+
+        public class DamageCalculator
+        {
+            public static float CaclDamage(CAR attacker, CAR target)
+            {
+                float damage = attacker.AttackPower - target.DefencePower;
+                if (damage < 1f)
+                    damage = 1f;
+
+                return damage;
+            }
+        }
+
+        
+        
+    }
+}
---
+++ ぐえんたん/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace GOGOGO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CAR car1 = new CAR(350f, 100f, 50f, 10f);
+            CAR car2 = new CAR(350f, 50f, 20f, 5f);
+
+            Console.WriteLine("Are you ready");
+            for (int i = 3; i > 0; i--)
+            {
+
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Start");
+
+            car1.Run(0);
+            Console.WriteLine("Car1Position" + " " + car1.Position + "(m)");
+            car2.Run(0);
+            Console.WriteLine("Car2Position" + " " + car2.Position + "(m)");
+
+
+
+            Console.WriteLine("Car1" + " " + car1.HP);
+            Console.WriteLine("Car2" + " " + car2.HP);
+
+            Console.WriteLine("---衝突---");
+            car1.Attack(car2);
+            car2.Attack(car1);
+
+            Console.WriteLine("Car1" + " " + car1.HP);
+            Console.WriteLine("Car2" + " " + car2.HP);
+
+
+            if (car1.HP == car2.HP)
+            {
+                Console.WriteLine("ゴールまで同時に来ます");
+            }
+
+            else if (car1.HP > car2.HP)
+            {
+                Console.WriteLine("Car2よりCar1は早くゴールまで");
+
+            }
+            else if (car1.HP < car2.HP)
+            {
+                Console.WriteLine("Car1よりCar2は早くゴールまで");
+            }
+
+
+        }
+    }
+}
